46c2e888e5a0486279a81469be282a6641004f0f
Michael <mike.segergmail.com>
2018-05-11 10:14:38 +0200

    first commit

---
+++ src/main/java/com/net128/app/gw/GwApplication.java
@@ -0,0 +1,12 @@
+package com.net128.app.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwApplication.class, args);
+	}
+}
